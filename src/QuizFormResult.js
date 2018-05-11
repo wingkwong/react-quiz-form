8b424078af2aef8039631237514419eb7755083f
wingkwong <wingkwong.code@gmail.com>
2018-05-11 13:32:02 +0800

    added copy to clipboard

--- src/QuizFormResult.js
+++ src/QuizFormResult.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class QuizFormResult extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      copied: false,
+    };
   }
 
   render() {
@@ -10,18 +14,41 @@ class QuizFormResult extends Component {
     const style = {
       resultBox: {
         background: '#e1f5fe',
-        minHeight: 300,
+        minHeight: 260,
         padding: 20,
         margin: 20
+      },
+      button: {
+        whiteSpace: 'nowrap',
+        display: 'inline-block',
+        borderRadius: '5px',
+        padding: '10px 15px',
+        margin: '0 0 20px 20px',
+        fontSize: '20px',
+        color: '#fff',
+        backgroundImage: 'linear-gradient(#4f93ce,#285f8f)',
+        border: '1px solid #285f8f',  
+        cursor: 'pointer'
       }
     }
 
     return (
       <div className="QuizFormResult">
        	  {result != null? (
-              <div style={style.resultBox}>
+              <div>
+                <div style={style.resultBox}>
                  <pre> {result} </pre>
               </div>
+              <div>
+                <CopyToClipboard text={result}
+                  onCopy={() => this.setState({copied: true})}>
+                  <button style={style.button}>Copy to Clipboard</button>
+                </CopyToClipboard> 
+              </div>
+
+              </div>
+              
+
           ) : (
             null
           )}
