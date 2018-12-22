d29ad3dd51241ddb236268ba953d2272000bf8a1
wingkwong <wingkwong.code@gmail.com>
2018-12-22 12:43:42 +0800

    fixed losing focus after 1 char issue / added messageForCorrectAnswer, messageForInorrectAnswer, and explanation fields

--- src/QuizForm.js
+++ src/QuizForm.js
@@ -5,116 +5,143 @@ import range from 'lodash/range'
 import validate from './validate';
 
 class QuizForm extends Component {
-  render() {
- 
- const {questionType} = this.props;
- const renderInputField = ({ input, label, type, meta: { touched, error } }) => (
-  <div>
-    <label>{label}</label>
+
+  renderInputField = ({ input, label, type, meta: { touched, error } }) => (
     <div>
-      <input {...input} type={type} placeholder={label} />
-      {touched && error && <span>{error}</span>}
+      <label>{label}</label>
+      <div>
+        <input {...input} type={type} placeholder={label} />
+        {touched && error && <span>{error}</span>}
+      </div>
     </div>
-  </div>
-);
+  );
 
-
-const renderSelectField = ({ input, label, type, meta: { touched, error }, children }) => (
-  <div>
-    <label>{label}</label>
+  renderTextareaField = ({ input, label, type, meta: { touched, error } }) => (
     <div>
-      <select {...input} >
-      	{children}
-      </select>
-      {touched && error && <span>{error}</span>}
+      <label>{label}</label>
+      <div>
+        <textarea {...input} type={type} placeholder={label} />
+        {touched && error && <span>{error}</span>}
+      </div>
     </div>
-  </div>
-);
+  );
 
-const renderSelectQuestionTypeField = ({ input, label, type, meta: { touched, error }, children }) => (
-  <div>
-    <label>{label}</label>
+   renderSelectField = ({ input, label, type, meta: { touched, error }, children }) => (
     <div>
-      <select {...input} >
-        {children}
-      </select>
-      {touched && error && <span>{error}</span>}
+      <label>{label}</label>
+      <div>
+        <select {...input} >
+          {children}
+        </select>
+        {touched && error && <span>{error}</span>}
+      </div>
     </div>
-  </div>
-);
-
-const renderTextAnswers = ({ fields, question, meta: { error } }) => (
-  <ul>
-    <li>
-      <button type="button" onClick={() => fields.push()}>Add Answer</button>
-    </li>
-    {fields.map((answer, index) => (
-      <li key={index}>
-        <button
-          type="button"
-          title="Remove Answer"
-          onClick={() => fields.remove(index)}
-        />
-        <Field
-          name={answer}
-          type="text"
-          component={renderInputField}
-          label={`Answer #${index + 1}`}
-        />
-      </li>
-    ))}
-        <li>
-       	<Field
-          name={`${question}.correctAnswer`}
-          component={renderSelectField}
-          label="Correct Answer"
-        >
-        	<option value="">Please select correct answer</option>
-	         {fields.map((answer, index) => (
-	         	<option key={index+1} value={index+1}>{`Answer #${index + 1}`}</option>
-	         ))}
-        </Field>
-        </li>
+  );
   
-    {error && <li className="error">{error}</li>}
-  </ul>
-);
-
-
-const renderQuestions = ({ fields, meta: { touched, error, submitFailed } }) => (
-  <ul>
-    <li>
-      <button type="button" onClick={() => fields.push({})}>Add Question</button>
-      {(touched || submitFailed) && error && <span>{error}</span>}
-    </li>
-    {fields.map((question, index) => (
-      <li key={index}>
-        <button
-          type="button"
-          title="Remove Question"
-          onClick={() => fields.remove(index)}
-        />
-        <h4>Question #{index + 1}</h4>
-        <Field
-          name={`${question}.question`}
-          type="text"
-          component={renderInputField}
-          label="Question Title"
-        />
-        <Field
-          name={`${question}.questionType`}
-          component={renderSelectQuestionTypeField}
-          label="Question Type"
-        >
-          <option value="">Please select a question type</option>
-          <option value="text">Text</option>
-          <option value="photo">Photo</option>
-        </Field>
-        <FieldArray name={`${question}.answers`} component={renderTextAnswers} question={question} />
+   renderSelectQuestionTypeField = ({ input, label, type, meta: { touched, error }, children }) => (
+    <div>
+      <label>{label}</label>
+      <div>
+        <select {...input} >
+          {children}
+        </select>
+        {touched && error && <span>{error}</span>}
+      </div>
+    </div>
+  );
+  
+   renderTextAnswers = ({ fields, question, meta: { error } }) => (
+    <ul>
+      <li>
+        <button type="button" onClick={() => fields.push()}>Add Answer</button>
       </li>
-    ))}
-  </ul>
-);
+      {fields.map((answer, index) => (
+        <li key={index}>
+          <button
+            type="button"
+            title="Remove Answer"
+            onClick={() => fields.remove(index)}
+          />
+          <Field
+            name={answer}
+            type="text"
+            component={this.renderInputField}
+            label={`Answer #${index + 1}`}
+          />
+        </li>
+      ))}
+          <li>
+           <Field
+            name={`${question}.correctAnswer`}
+            component={this.renderSelectField}
+            label="Correct Answer"
+          >
+            <option value="">Please select correct answer</option>
+             {fields.map((answer, index) => (
+               <option key={index+1} value={index+1}>{`Answer #${index + 1}`}</option>
+             ))}
+          </Field>
+          </li>
+    
+      {error && <li className="error">{error}</li>}
+    </ul>
+  );
+  
+  
+   renderQuestions = ({ fields, meta: { touched, error, submitFailed } }) => (
+    <ul>
+      <li>
+        <button type="button" onClick={() => fields.push({})}>Add Question</button>
+        {(touched || submitFailed) && error && <span>{error}</span>}
+      </li>
+      {fields.map((question, index) => (
+        <li key={index}>
+          <button
+            type="button"
+            title="Remove Question"
+            onClick={() => fields.remove(index)}
+          />
+          <h4>Question #{index + 1}</h4>
+          <Field
+            name={`${question}.question`}
+            type="text"
+            component={this.renderInputField}
+            label="Question Title"
+          />
+          <Field
+            name={`${question}.questionType`}
+            component={this.renderSelectQuestionTypeField}
+            label="Question Type"
+          >
+            <option value="">Please select a question type</option>
+            <option value="text">Text</option>
+            <option value="photo">Photo</option>
+          </Field>
+          <FieldArray name={`${question}.answers`} component={this.renderTextAnswers} question={question} />
+          <Field
+            name={`${question}.messageForCorrectAnswer`}
+            type="text"
+            component={this.renderTextareaField}
+            label="Message for Correct Answer"
+          />
+          <Field
+            name={`${question}.messageForInorrectAnswer`}
+            type="text"
+            component={this.renderTextareaField}
+            label="Message for Incorrect Answer"
+          />
+          <Field
+            name={`${question}.explanation`}
+            type="text"
+            component={this.renderTextareaField}
+            label="Explanation"
+          />
+        </li>
+      ))}
+    </ul>
+  );
+
+  render() {
 
   const { handleSubmit, pristine, reset, submitting } = this.props;
 
@@ -124,10 +151,10 @@ const renderQuestions = ({ fields, meta: { touched, error, submitFailed } }) => 
 	      <Field
 	        name="quizTitle"
 	        type="text"
-	        component={renderInputField}
+	        component={this.renderInputField}
 	        label="Quiz Title"
 	      />
-	      <FieldArray name="questions" component={renderQuestions} />
+	      <FieldArray name="questions" component={this.renderQuestions} />
 	      <div>
 	        <button type="submit" disabled={submitting}>Submit</button>
 	        <button type="button" disabled={pristine || submitting} onClick={reset}>
